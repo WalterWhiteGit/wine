bfe74ae2ee4e2233ec3ce943aa53e2855f32226e
WalterWhiteGit <walterwhitegithub@gmail.com>
2017-11-07 20:01:39 +0100

    Mise à jour

---
+++ src/AppBundle/Subscriber/ContactEventSubscriber.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samy
+ * Date: 06/11/2017
+ * Time: 12:04
+ */
+
+namespace AppBundle\Subscriber;
+
+
+use AppBundle\Event\ContactMailEvent;
+use AppBundle\Event\ContactMailSendMail;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ContactEventSubscriber implements EventSubscriberInterface
+{
+
+
+    private $mailer;
+    private $doctrine;
+
+
+
+    public function __construct(ManagerRegistry $doctrine, \Swift_Mailer $mailer)
+    {
+
+        $this->mailer=$mailer;
+        $this->doctrine=$doctrine;
+
+    }
+
+    public static function getSubscribedEvents()
+    {
+        // Call Event from AppBundle\Event
+        return [ContactMailEvent::Send_Mail_Contact=>'sendContactMail'];
+
+    }
+
+
+    public function sendContactMail(ContactMailSendMail $event, $mailer)
+    {
+
+        // Get variables
+
+        $mail = $event->getEmail();
+        $firstname = $event->getFirstname();
+
+
+
+        //
+        $sendmail = (new \Swift_Message())
+
+                ->setFrom('wine@gmail.com')
+                ->setSubject('Demande de contact de '.$firstname)
+                ->setTo($mail)
+                ->setContentType('Text/Html')
+                ->setBody('Test reussi');
+
+
+
+
+        // Send mail
+        $this->mailer->send($sendmail);
+
+
+    }
+
+}
